7b2a6251e75f9c88c1c1f3a2562db99238b2bde5
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2015-06-12 20:35:20 +0300

    save fields as strings

--- dirbot/spiders/dmoz.py
+++ dirbot/spiders/dmoz.py
@@ -4,6 +4,10 @@ from scrapy.selector import Selector
 
 from dirbot.items import Apartment
 
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
 
 class DmozSpider(Spider):
     name = "dmoz"
@@ -11,6 +15,12 @@ class DmozSpider(Spider):
     start_urls = [
         "http://moscow.thelocals.ru/rooms#/?page=1&search[min_price]=3750&search[max_price]=40000&search[only_rooms]=false&search[animals_allowed]=false&search[lat_min]=55.259962662285524&search[lat_max]=56.070317310814076&search[lng_min]=37.164561266992855&search[lng_max]=38.09977244863348&order=date",
     ]
+
+    def _save_item(self, item, selector, field_name, path):
+        try:
+            item[field_name] = selector.xpath(path).extract()[0]
+        except IndexError:
+            item[field_name] = ''
 
     def parse(self, response):
         """
@@ -28,13 +38,13 @@ class DmozSpider(Spider):
         for apartment in apartments:
             item = Apartment()
             apartment_sel = Selector(text=apartment.extract())
-            item['url'] = apartment_sel.xpath('//a/@href').extract()
-            item['time'] = apartment_sel.xpath('//span[@class="catalog-item-time"]//text()').extract()
-            item['title'] = apartment_sel.xpath('//div[@class="title"]//text()').extract()
-            item['image'] = apartment_sel.xpath('//img/@src').extract()
-            item['rooms'] = apartment_sel.xpath('(//div[@class="catalog-item-stats-item-value"])[1]//text()').extract()
-            item['area'] = apartment_sel.xpath('(//div[@class="catalog-item-stats-item-value"])[2]//text()').extract()
-            item['price'] = apartment_sel.xpath('(//div[@class="catalog-item-stats-item-value"])[3]//text()').extract()
-            item['metro_station'] = apartment_sel.xpath('(//span)[last()]//text()').extract()
+            self._save_item(item, apartment_sel, 'url', '//a/@href')
+            self._save_item(item, apartment_sel, 'time', '//span[@class="catalog-item-time"]//text()')
+            self._save_item(item, apartment_sel, 'title', '//div[@class="title"]//text()')
+            self._save_item(item, apartment_sel, 'image', '//img/@src')
+            self._save_item(item, apartment_sel, 'rooms', '(//div[@class="catalog-item-stats-item-value"])[1]//text()')
+            self._save_item(item, apartment_sel, 'area', '(//div[@class="catalog-item-stats-item-value"])[2]//text()')
+            self._save_item(item, apartment_sel, 'price', '(//div[@class="catalog-item-stats-item-value"])[3]//text()')
+            self._save_item(item, apartment_sel, 'metro_station', '(//span)[last()]//text()')
             items.append(item)
         return items
